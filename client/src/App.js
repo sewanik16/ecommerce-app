b88fdb48128217501c79f3c2107f5a8e9edab43c
Nikhil <devsotnik@gmail.com>
2022-08-20 19:24:23 +0530

    new start for ecoom + edashoad app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 
-import Signup from "./components/signup/signup";
-import Login from "./components/login/login";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
 
 const App = () =>{
   return (
     <>
-        <h2>E-Commerce App</h2>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<Signup/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-          </Routes>
-        </BrowserRouter>
+
+       
     </>
   )
 }
