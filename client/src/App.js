5c807bbe8a99507d5cf1961b80ff6dd0bee52beb
Nikhil <devsotnik@gmail.com>
2022-08-21 08:08:12 +0530

    basic home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 
-
-
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () =>{
   return (
     <>
-
-       
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,2 +1,43 @@
 
+import { Navbar,Nav, Container, Image } from "react-bootstrap";
 
+import {LinkContainer } from "react-router-bootstrap";
+
+const NavBar = () => {
+  
+  return (
+    <>
+      <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark" style={{height:"100px"}}>
+        <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <Image
+              src="images/logo11.png" 
+              alt="logo" className="App-logo"
+              style={{ height: "150px" }}
+            />
+          </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+           
+             <Nav className="ms-auto">
+                  
+             <LinkContainer to="/cart">
+             <Nav.Link>Cart</Nav.Link>
+             </LinkContainer>
+            
+             <LinkContainer to="/login">
+             <Nav.Link>Login</Nav.Link>
+             </LinkContainer>
+             
+             </Nav>
+                
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
